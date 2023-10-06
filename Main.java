efb20d1b8e49c0ed41c0fc139292fce1eb2e554b
binjamingotbenz <147190849+binjamingotbenz@users.noreply.github.com>
2023-10-06 23:47:49 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Степан";
+        int age = 18;
+        float weight = 69.7f;
+        System.out.printf("Name: %s  Age: %d  Weight: %.1f \n", name, age, weight);
+    }
+}
