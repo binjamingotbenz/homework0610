66973839064a7f6c4b008d445b64f5805c1e3636
binjamingotbenz <147190849+binjamingotbenz@users.noreply.github.com>
2023-10-07 13:50:08 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        String name = "Степан";
-        int age = 18;
-        float weight = 69.7f;
-        System.out.printf("Name: %s  Age: %d  Weight: %.1f \n", name, age, weight);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = in.nextLine();
+        System.out.print("Введите свой возраст: ");
+        int age = in.nextInt();
+        System.out.print("Введите свой рост: ");
+        float weight = in.nextFloat();
+
+        System.out.printf("Имя: %s Возраст: %d Вес: %.1f \n", name, age, weight);
     }
 }
